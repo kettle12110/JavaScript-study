37e670419aaff8716cd9e35c10fb3c50cbcbcad2
kettle12110 <kettle12110@gmail.com>
2024-05-09 20:10:20 +0900

    なんかうまくいかない

--- 13-demo/js/script.js
+++ 13-demo/js/script.js
@@ -1,5 +1,11 @@
 const getScrollPercent=()=>{
-    console.log('スクロールされました');
-}
+    const scrolled = window.scrollY;
+    console.log('${scrolled} スクロールされました');
 
+const pageHeight = document.documentElement.scrollHeight;
+
+const viewHeight = document.documentElement.clientHeight;
+
+console.log('ページの高さ:${pageHeight}、表示領域の高さ:${viewHeight}')
+}
 window.addEventListener('scroll',getScrollPercent);
