43baeec5b5992c79d8adc18ea257aa4438e1d9a3
kettle12110 <kettle12110@gmail.com>
2024-05-13 20:08:06 +0900

    シングルクォートとバッククォート間違えてた

--- 13-demo/js/script.js
+++ 13-demo/js/script.js
@@ -1,11 +1,11 @@
 const getScrollPercent=()=>{
     const scrolled = window.scrollY;
-    console.log('${scrolled} スクロールされました');
+    console.log(`${scrolled} スクロールされました`);
 
 const pageHeight = document.documentElement.scrollHeight;
 
 const viewHeight = document.documentElement.clientHeight;
 
-console.log('ページの高さ:${pageHeight}、表示領域の高さ:${viewHeight}')
+console.log(`ページの高さ:${pageHeight}、表示領域の高さ:${viewHeight}`)
 }
 window.addEventListener('scroll',getScrollPercent);
