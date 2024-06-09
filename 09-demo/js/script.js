bfe3be8b2cce8cbdce02de88a78ee33ed7d27a32
kettle12110 <kettle12110@gmail.com>
2024-06-09 12:50:18 +0900

    きりんさんがふわっと動くぜ

---
+++ 09-demo/js/script.js
@@ -0,0 +1,10 @@
+const showkirin=(entries)=>{
+    const keyframes = {
+        opacity:[0,1],
+        translate:['200px 0',0],
+    }
+    entries[0].target.animate(keyframes,600);
+};
+
+const kirinObserver= new IntersectionObserver(showkirin);
+kirinObserver.observe(document.querySelector('#kirin'));
