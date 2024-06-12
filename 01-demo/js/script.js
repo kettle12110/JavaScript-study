8a4d3a092ac12341cfc9954da7e8e8994e16359c
kettle12110 <kettle12110@gmail.com>
2024-06-13 08:58:00 +0900

    コンソールで取得確認してから動かすのがいいっぽいな

--- 01-demo/js/script.js
+++ 01-demo/js/script.js
@@ -67,3 +67,67 @@ thumbImages.forEach((thumbImage)=>{
         mainImage.animate({opacity:[0,1]},500);
     });
 });
+
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li');
+const menuOptions ={
+    duration:1400,
+    easing:'ease',
+    fill:'forwards',
+};
+
+//メニューを開く
+menuOpen.addEventListener('click',()=>{
+    menuPanel.animate({translate:['100vw',0]},menuOptions);
+//リンクをひとつずつ順に表示
+menuItems.forEach((menuItem,index)=>{
+    menuItem.animate(
+        {
+            opacity:[0,1],
+            translate:['2rem',0],
+        },
+        {
+            duration:2400,
+            delay:300 *index,
+            easing:'ease',
+            fill:'forwards',
+        }
+    );
+});
+});
+
+//メニューを閉じる
+menuClose.addEventListener('click',()=>{
+    menuPanel.animate({translate:[0,'100vw']},menuOptions);
+    menuItems.forEach((menuItem)=>{
+    menuItem.animate({opacity:[1,0]},menuOptions);
+});
+});
+//監視対象が範囲内に現れたら実行する動作
+const animateFade=(entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+        entry.target.animate(
+            {
+                opacity:[0,1],
+                filter:['blur(.4rem)','blur(0)'],
+                translate:['0 4rem',0]
+            },
+            {
+                duration:2000,
+                easing: 'ease',
+                fill:'forwards',
+            }
+        );
+        }
+    })
+}
+//監視設定
+const fadeObserver= new IntersectionObserver(animateFade);
+//.fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement)=>{
+    fadeObserver.observe(fadeElement);
+})
